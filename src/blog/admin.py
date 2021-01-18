80ced2955d971f7e0cc0b61ad7a8314d495bf14a
raymondaksu <raymondaksu@users.noreply.github.com>
2021-01-18 22:16:04 +0300

    Created blog app model

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.db import models
+import nested_admin
+from .models import Category, Post, Comment, Like, PostView
 
-# Register your models here.
+class PostViewInline(nested_admin.NestedTabularInline):
+    model = PostView
+    classes = ['collapse']
+
+class LikeInline(nested_admin.NestedTabularInline):
+    model = Like
+    classes = ['collapse']
+
+class CommentInline(nested_admin.NestedTabularInline):
+    model = Comment
+    classes = ['collapse']
+
+class PostAdmin(nested_admin.NestedModelAdmin):
+    model = Post
+    inlines = [PostViewInline, LikeInline, CommentInline]
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(PostView)
